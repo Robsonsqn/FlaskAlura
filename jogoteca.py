cb3668e13d373360f9094b6fb816a9561cba734f
robsonsqn <robson.souza@fncit.com.br>
2019-03-10 23:56:33 -0300

    Get information from 'Server'
    #PraBrasileiroLer
    Pegando informações do 'Server'

--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
 
+from domain.domains import Jogo
 
 app = Flask(__name__)
 
 
+def jogos_padrao():
+    return [
+        Jogo('Tetris', 'Arcade', 'Atari'),
+        Jogo('Mario', 'Plataforma', 'SNES'),
+        Jogo('Pokemon Lets Go', 'RPG', 'Switch')
+    ]
+
+
 @app.route('/home')
 def home():
-    return render_template('lista.html', titulo='JOGOS')
+    lista_jogos = jogos_padrao()
+    return render_template('lista.html', titulo='JOGOS', jogos=lista_jogos)
 
 app.run()
