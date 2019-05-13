2974d50bb52fc16fcad629bd684daf46839c99ae
robson_sqn <robson.souza@fncit.com.br>
2019-05-13 10:53:25 -0300

    [RGS] IDK

--- jogoteca.py
+++ jogoteca.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template
-
 from domain.domains import Jogo
-
 app = Flask(__name__)
 
 
@@ -18,4 +16,16 @@ def home():
     lista_jogos = jogos_padrao()
     return render_template('lista.html', titulo='JOGOS', jogos=lista_jogos)
 
-app.run()
+
+@app.route('/novo')
+def novo():
+    return render_template('cadastro.html', titulo='Novo Jogo'),
+
+
+def main():
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
+
